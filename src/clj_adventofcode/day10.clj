0ac99db9a75e71a162a56428a0d47184d2394722
Sascha Koch <sascha.koch@web.de>
2018-01-05 23:29:56 +0100

    day 10, part 2 dense-hash implemented

--- src/clj_adventofcode/day10.clj
+++ src/clj_adventofcode/day10.clj
@@ -1,55 +1,58 @@
-(ns clj-adventofcode.day10)
+(ns clj-adventofcode.day10
+  (:require [clojure.string :as str]))
 
-(defn shift-right
-  [ls n]
-  (loop [shift-ls ls
-         cnt n]
-    (if (zero? cnt)
-      shift-ls
-      (let [tail (last shift-ls)
-            head (take (dec (count ls)) shift-ls)]
-        (recur (conj head tail) (dec cnt))))))
+(defn shift-right [ls n]
+  (let [dn (- (count ls) (mod n (count ls)))]
+    (concat (drop dn ls) (take dn ls))))
 
-(defn shift-left
-  [ls n]
-  (loop [shift-ls (vec ls)
-         cnt n]
-    (if (zero? cnt)
-      (lazy-seq shift-ls)
-      (let [tail (first shift-ls)
-            head (vec (drop 1 shift-ls))]
-        (recur (conj head tail) (dec cnt))))))
+(defn shift-left [ls n]
+  (let [dn (mod n (count ls))]
+    (concat (drop dn ls) (take dn ls))))
 
-(defn reverse-section
-  [l offset length]
-  (let [shifted-ls (shift-left l offset)
-        sublist (take length shifted-ls)
-        restlist (drop length shifted-ls)
-        reversed-sublist (reverse sublist)
-        new-shifted-list (concat reversed-sublist restlist)
-        new-list (shift-right new-shifted-list offset)]
-    new-list))
+(defn reverse-section [ls off len]
+  (let [lls (shift-left ls off)]
+    (shift-right
+      (concat
+        (reverse (take len lls))
+        (drop len lls)) off)))
 
-(defn step
-  [ls current-pos skip-size length]
-  (let [steps (+ skip-size length)
-        new-ls (reverse-section ls current-pos length)
-        new-pos (mod (+ current-pos steps) (count ls))
-        new-skip-size (inc skip-size)]
-    [new-ls new-pos new-skip-size]))
-
-(defn knot-hash
-  [clist input]
-  (loop [[ls pos skip-size] [clist 0 0]
+(defn run-round [input ls pos ss]
+  (loop [l ls
+         p pos
+         s ss
          [i & is] input]
     (if (nil? i)
-      (reduce * 1 (take 2 ls))
-      (recur (step ls pos skip-size i) is))))
+      [l p s]
+      (recur
+        (reverse-section l p i)
+        (mod (+ p (+ s i)) (count l))
+        (inc s)
+        is))))
+
+(defn knot-hash [ls input]
+  (reduce * 1 (take 2 (first (run-round input ls 0 0)))))
+
+(defn sparse-hash [ls input]
+  (loop [[l pos skip-size] [ls 0 0]
+         cnt 0]
+    (if (>= cnt 64)
+      l
+      (recur (run-round input l pos skip-size) (inc cnt)))))
+
+(defn dense-hash [ls input]
+  (str/join
+    (map #(format "%02x" %)
+      (map #(apply bit-xor %)
+        (partition 16
+          (sparse-hash ls
+            (concat (map int input) '(17 31 73 47 23))))))))
 
 (def standard-list (range 256))
+(def puzzle-input "46,41,212,83,1,255,157,65,139,52,39,254,2,86,0,204")
 
-(def puzzle-input '(46 41 212 83 1 255 157 65 139 52 39 254 2 86 0 204))
-;; 46,41,212,83,1,255,157,65,139,52,39,254,2,86,0,204
-
-(println "Day 10, Part 1: " (knot-hash standard-list puzzle-input))
+(println "Day 10, Part 1: "
+         (knot-hash standard-list (map #(Integer/parseInt %) (str/split puzzle-input #","))))
 ;; 52070
+(println "Day 10, Part 2: "
+         (dense-hash standard-list puzzle-input))
+;; 7f94112db4e32e19cf6502073c66f9bb
--- test/clj_adventofcode/day10_test.clj
+++ test/clj_adventofcode/day10_test.clj
@@ -2,6 +2,12 @@
   (:require [clojure.test :refer :all]
             [clj-adventofcode.day10 :refer :all]))
 
-(deftest test-case-day10
-  (testing "..."
-    (is true)))
+(deftest dense-hash-test
+  (testing "The empty string becomes a2582a3a0e66e6e86e3812dcb672a272."
+    (is (= "a2582a3a0e66e6e86e3812dcb672a272" (dense-hash standard-list ""))))
+  (testing "AoC 2017 becomes 33efeb34ea91902bb2f59c9920caa6cd."
+    (is (= "33efeb34ea91902bb2f59c9920caa6cd" (dense-hash standard-list "AoC 2017"))))
+  (testing "1,2,3 becomes 3efbe78a8d82f29979031a4aa0b16a9d."
+    (is (= "3efbe78a8d82f29979031a4aa0b16a9d" (dense-hash standard-list "1,2,3"))))
+  (testing "1,2,4 becomes 63960835bcdc130f0b66d7ff4f6a5a8e."
+    (is (= "63960835bcdc130f0b66d7ff4f6a5a8e" (dense-hash standard-list "1,2,4")))))
